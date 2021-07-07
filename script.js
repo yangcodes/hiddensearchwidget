78db702131665c3629b9fdd1929d2a4d07d6e38c
Jenny Lin <misslinyang91@gmail.com>
2021-07-07 15:28:24 +1000

    final touches on search bar

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const search = document.querySelector(".search");
 const btn = document.querySelector(".btn");
-
 const input = document.querySelector(".input");
 
 btn.addEventListener("click", () => {
